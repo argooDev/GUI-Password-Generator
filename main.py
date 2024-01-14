d378540db97b64161af9f87c6aa114537c121590
argo-dev <gorshechnikof@gmail.com>
2024-01-14 15:56:02 +0500

    Finally logic, allows to generate password by length entered user

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter
 import tkinter.messagebox
+import random
 
 
 class MainGUI:
@@ -15,18 +16,18 @@ class MainGUI:
             )
         self.intro.pack(ipadx=20, ipady=20, side='top')
 
-        # This code counting password
-        self.count_frame = tkinter.Frame(self.main_window)
-        self.pass_count = tkinter.Label(
-            self.count_frame,
-            text='How many password you want to generate?'
-        )
-        self.pass_count.pack(side='left')
-        self.entry_pass_count = tkinter.Entry(
-            self.count_frame,
-            width=15
-        )
-        self.entry_pass_count.pack(side='left')
+        # # This code counting password
+        # self.count_frame = tkinter.Frame(self.main_window)
+        # self.pass_count = tkinter.Label(
+        #     self.count_frame,
+        #     text='How many password you want to generate?'
+        # )
+        # self.pass_count.pack(side='left')
+        # self.entry_pass_count = tkinter.Entry(
+        #     self.count_frame,
+        #     width=15
+        # )
+        # self.entry_pass_count.pack(side='left')
 
         # This code load length of password
         self.len_frame = tkinter.Frame(self.main_window)
@@ -42,78 +43,94 @@ class MainGUI:
         self.entry_len_pass.pack(side='left')
 
         # Use digits?
-        self.digits_frame = tkinter.Frame(self.main_window)
+        # self.digits_frame = tkinter.Frame(self.main_window)
+        #
+        # self.digits = tkinter.Label(
+        #     self.digits_frame,
+        #     text='0123456789'
+        # )
+        # self.digits.pack(side='left')
+        #
+        # self.use_digits_value = tkinter.IntVar()
+        # self.u_digits = tkinter.Checkbutton(
+        #     self.digits_frame,
+        #     text='Add',
+        #     variable=self.use_digits_value
+        # )
+        # self.u_digits.pack(side='left')
+        #
+        # # Use string char?
+        # self.str_char_frame = tkinter.Frame(self.main_window)
+        #
+        # self.str_label = tkinter.Label(
+        #     self.str_char_frame,
+        #     text='abcdefghijklmnopqrstuvwxyz'
+        # )
+        # self.str_label.pack(side='left')
+        #
+        # self.str_value = tkinter.IntVar()
+        # self.u_str = tkinter.Checkbutton(
+        #     self.str_char_frame,
+        #     text='Add',
+        #     variable=self.str_value
+        # )
+        # self.u_str.pack(side='left')
+        #
+        # # Use string char upper?
+        # self.str_char_upper_frame = tkinter.Frame(self.main_window)
+        #
+        # self.str_upper = tkinter.Label(
+        #     self.str_char_upper_frame,
+        #     text='ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+        # )
+        # self.str_upper.pack(side='left')
+        #
+        # self.str_upper_value = tkinter.IntVar()
+        # self.u_str_upper = tkinter.Checkbutton(
+        #     self.str_char_upper_frame,
+        #     text='Add',
+        #     variable=self.str_upper_value
+        # )
+        # self.u_str_upper.pack(side='left')
+        #
+        # # Use punctuation?
+        # self.punctuation_frame = tkinter.Frame(self.main_window)
+        #
+        # self.punc_label = tkinter.Label(
+        #     self.punctuation_frame,
+        #     text='!#$%&*+-=?@^_'
+        # )
+        # self.punc_label.pack(side='left')
+        #
+        # self.punc_value = tkinter.IntVar()
+        # self.use_punc = tkinter.Checkbutton(
+        #     self.punctuation_frame,
+        #     text='Add',
+        #     variable=self.punc_value
+        # )
+        # self.use_punc.pack(side='left')
 
-        self.digits = tkinter.Label(
-            self.digits_frame,
-            text='0123456789'
+        #This code show generated password
+        self.result_password_frame = tkinter.Frame(self.main_window)
+        self.result_password = tkinter.Label(
+            self.result_password_frame,
+            text='Password: '
         )
-        self.digits.pack(side='left')
+        self.result_password.pack(side='left')
 
-        self.use_digits_value = tkinter.IntVar()
-        self.u_digits = tkinter.Checkbutton(
-            self.digits_frame,
-            text='Add',
-            variable=self.use_digits_value
+        self.password = tkinter.StringVar()
+        self.password_label = tkinter.Entry(
+            self.result_password_frame,
+            textvariable=self.password
         )
-        self.u_digits.pack(side='left')
-
-        # Use string char?
-        self.str_char_frame = tkinter.Frame(self.main_window)
-
-        self.str_label = tkinter.Label(
-            self.str_char_frame,
-            text='abcdefghijklmnopqrstuvwxyz'
-        )
-        self.str_label.pack(side='left')
-
-        self.str_value = tkinter.IntVar()
-        self.u_str = tkinter.Checkbutton(
-            self.str_char_frame,
-            text='Add',
-            variable=self.str_value
-        )
-        self.u_str.pack(side='left')
-
-        # Use string char upper?
-        self.str_char_upper_frame = tkinter.Frame(self.main_window)
-
-        self.str_upper = tkinter.Label(
-            self.str_char_upper_frame,
-            text='ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-        )
-        self.str_upper.pack(side='left')
-
-        self.str_upper_value = tkinter.IntVar()
-        self.u_str_upper = tkinter.Checkbutton(
-            self.str_char_upper_frame,
-            text='Add',
-            variable=self.str_upper_value
-        )
-        self.u_str_upper.pack(side='left')
-
-        # Use punctuation?
-        self.punctuation_frame = tkinter.Frame(self.main_window)
-
-        self.punc_label = tkinter.Label(
-            self.punctuation_frame,
-            text='!#$%&*+-=?@^_'
-        )
-        self.punc_label.pack(side='left')
-
-        self.punc_value = tkinter.IntVar()
-        self.use_punc = tkinter.Checkbutton(
-            self.punctuation_frame,
-            text='Add',
-            variable=self.punc_value
-        )
-        self.use_punc.pack(side='left')
+        self.password_label.pack(side='left')
 
         # This is main buttons
         self.bottom_frame = tkinter.Frame(self.main_window)
         self.generate_btn = tkinter.Button(
             self.bottom_frame,
-            text='Generate'
+            text='Generate',
+            command=self.generate_password
         )
         self.generate_btn.pack(side='left')
         self.exit_btn = tkinter.Button(
@@ -123,15 +140,32 @@ class MainGUI:
         )
         self.exit_btn.pack(side='left')
 
-        self.count_frame.pack()
+        #self.count_frame.pack()
         self.len_frame.pack()
-        self.digits_frame.pack()
-        self.str_char_frame.pack()
-        self.str_char_upper_frame.pack()
-        self.punctuation_frame.pack()
+        #self.digits_frame.pack()
+        #self.str_char_frame.pack()
+        #self.str_char_upper_frame.pack()
+        #self.punctuation_frame.pack()
+        self.result_password_frame.pack()
         self.bottom_frame.pack()
 
         tkinter.mainloop()
+
+    def generate_password(self):
+        #self.password_count = int(self.entry_pass_count.get())
+        self.length_password = int(self.entry_len_pass.get())
+        self.digits_val = '0123456789'
+        self.punctuation = '!#$%&*+-=?@^_'
+        self.l_char = 'abcdefghijklmnopqrstuvwxyz'
+        self.h_char = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+        self.password_templates = list(self.digits_val + self.punctuation + self.l_char + self.h_char)
+        self.storage = []
+        for password in range(self.length_password):
+            self.storage.append(random.choice(self.password_templates))
+
+        self.result_password_value = ''.join(self.storage)
+        self.password.set(self.result_password_value)
 
 
 if __name__ == '__main__':
